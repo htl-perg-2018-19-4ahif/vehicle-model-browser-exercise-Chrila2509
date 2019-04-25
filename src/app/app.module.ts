5391a12f09cfbe4df9e0dbf8a24270078604d45c
DeaGiXe <if140049@htblaperg.onmicrosoft.com>
2019-04-25 14:05:27 +0200

    basic angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ModelComponent } from './model/model.component';
+import { DataTableComponent } from './data-table/data-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ModelComponent,
+    DataTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
